d07d3ea0277613e68f4104eb41830503bbbb8992
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-09 14:15:01 -0500

    Brainstorm start of game logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const gameBoard = (function () {
+  let board = [['.', '.', '.'],
+    ['.', '.', '.'],
+    ['.', '.', '.']];
+  const set = (x, y, symbol) => {
+    board[x][y] = symbol;
+  };
+  const display = () => {
+    console.log(board);
+  };
+  return { set, display };
+}());
+
+const player = (symbol) => ({ symbol });
+
+const player1 = player('X');
+const player2 = player('O');
 
