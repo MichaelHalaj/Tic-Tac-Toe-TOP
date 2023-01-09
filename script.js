af6690a5ca94c79183e0396b817f17d2c22a3916
Michael Halaj <michaelhalaj7@gmail.com>
2023-01-09 15:32:18 -0500

    Add checks for winner

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 const gameBoard = (() => {
+  const n = 3;
+  let moveCount = 0;
   let board = [
     [".", ".", "."],
     [".", ".", "."],
     [".", ".", "."],
   ];
-  const draw = () => {};
+  const drawBoard = () => {};
+  const checkBoard = (x, y, symbol) => {
+    for (let i = 0; i < n; i++) {
+      if (board[x][i] !== symbol) {
+        break;
+      }
+      if (i === n - 1) {
+        //return winner
+      }
+    }
+    for (let i = 0; i < n; i++) {
+      if (board[i][y] !== symbol) {
+        break;
+      }
+      if (i === n - 1) {
+        //return winner
+      }
+    }
+    if (x === y) {
+      for (let i = 0; i < n; i++) {
+        if (board[i][i] !== symbol) {
+          break;
+        }
+        if (i === n - 1) {
+          //return winner
+        }
+      }
+    }
+    if (x + y === n - 1) {
+      for (let i = 0; i < n; i++) {
+        if (board[i][n - 1 - i] !== symbol) {
+          break;
+        }
+        if (i === n - 1) {
+          //return winner
+        }
+      }
+    }
+  };
   const set = (x, y, symbol) => {
-    board[x][y] = symbol;
+    if (board[x][y] === ".") {
+      board[x][y] = symbol;
+    }
+    checkBoard();
   };
   const display = () => {
     console.log(board);
